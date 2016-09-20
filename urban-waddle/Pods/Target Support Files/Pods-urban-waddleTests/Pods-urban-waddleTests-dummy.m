8a11d795280abfebbe31c068fc1f945b2e150d99
dingjun <wwb@wwbtekimacbook-pro-2.local>
2016-09-20 14:59:37 +0800

    masonry

---
+++ urban-waddle/Pods/Target Support Files/Pods-urban-waddleTests/Pods-urban-waddleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_urban_waddleTests : NSObject
+@end
+@implementation PodsDummy_Pods_urban_waddleTests
+@end
---
+++ urban-waddle/Pods/Target Support Files/Pods-urban-waddleUITests/Pods-urban-waddleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_urban_waddleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_urban_waddleUITests
+@end
